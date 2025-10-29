3051111ee7eb98cfb2b090bf93f1b75cf6b4768f
matt <mhyatt@luc.edu>
2025-10-29 22:06:49 +0000

    fix import issues

--- xgym/__init__.py
+++ xgym/__init__.py
@@ -2,11 +2,12 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from xgym import calibrate, utils, viz
 from xgym.calibrate import urdf
 from xgym.calibrate.urdf import robot
 from xgym.names import *  # noqa
 from xgym.utils import logger
+
+from . import calibrate, utils, viz
 
 try:
     from xgym import nodes
